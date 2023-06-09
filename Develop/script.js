c4730b9e43fe5f8d49628aacd0102f480025dd3a
Christine Lea <christine87@live.com.au>
2023-06-09 20:29:34 +1000

    "test js code if statements using console.log"

--- Develop/script.js
+++ Develop/script.js
@@ -15,42 +15,42 @@ function generatePassword() {
 	);
 
 	var passwordLowerCase = window.confirm(
-		"Include \n\nlowercase letters? \n\nSelect OK for Yes or Cancel for No"
+		"\nInclude lowercase letters? \nOK for Yes or Cancel for No"
 	);
   if (passwordLowerCase) {
     randomPasswordString = randomPasswordString.concat(lowerCase);
-    console.log(randomPasswordString);
+    // console.log(randomPasswordString);
 	}
 
 	var passwordUpperCase = window.confirm(
-		"Include \n\nUPPERCASE letters? \n\nSelect OK for Yes or Cancel for No"
+		"\nInclude UPPERCASE letters? \nOK for Yes or Cancel for No"
 	);
 	if (passwordUpperCase) {
     randomPasswordString = randomPasswordString.concat(upperCase);
-    console.log(randomPasswordString);
+    // console.log(randomPasswordString);
 	}
 
 	var passwordNumbers = window.confirm(
-		"Include Numbers? \n\n OK for Yes or Cancel for No"
+		"\nInclude Numbers? \nOK for Yes or Cancel for No"
 	);
 	if (passwordNumbers) {
     randomPasswordString = randomPasswordString.concat(numbers);
-    console.log(randomPasswordString);
+    // console.log(randomPasswordString);
 	}
 
 	var passwordSpecialCharacters = window.confirm(
-		"Inlcude Special Characters? \n\nOK for Yes or Cancel for No"
+		"\nInlcude Special Characters? \n\nOK for Yes or Cancel for No"
 	);
   if (passwordSpecialCharacters) {
     randomPasswordString = randomPasswordString.concat(specialCharacters);
-		console.log(randomPasswordString);
+		// console.log(randomPasswordString);
 	} else {
 		alert("Did not meet password criteria. Please start again.");
 		return;
 	}
 
 	var passwordLength = window.prompt(
-		"Password length must be between 8 - 128 characters. \nEnter Password Length:"
+		"\nPassword length must be between 8 - 128 characters. \nEnter Password Length:"
 	);
 
 	if (passwordLength >= 8 && passwordLength <= 128) {
@@ -62,14 +62,14 @@ function generatePassword() {
 		return;
   }
   
+  for (var i = 0; i < randomPasswordString; i++) {
+
+  }
+
 };
 
 
-// join arrays into one array based on criteria that returns true
-// for loop to loop over array and randomly output array elements to match # of characters
-// .toString() to convert into a string
-// output password to generator
-// clear password
+
 
 // Write password to the #password input
 function writePassword() {
