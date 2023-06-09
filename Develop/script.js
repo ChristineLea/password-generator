ada0f9607d4ec4387ad8968034c8eb2faee5a423
Christine Lea <christine87@live.com.au>
2023-06-09 21:03:30 +1000

    "return value from prompt working"

--- Develop/script.js
+++ Develop/script.js
@@ -6,8 +6,8 @@ var upperCase = lowerCase.toUpperCase();
 var numbers = "1234567890";
 var specialCharacters = "!@#$%&*_";
 
-var length = 0;
 var randomPasswordString = "";
+var length = 0;
 
 function generatePassword() {
 	window.alert(
@@ -17,32 +17,32 @@ function generatePassword() {
 	var passwordLowerCase = window.confirm(
 		"\nInclude lowercase letters? \nOK for Yes or Cancel for No"
 	);
-  if (passwordLowerCase) {
-    randomPasswordString = randomPasswordString.concat(lowerCase);
-    // console.log(randomPasswordString);
+	if (passwordLowerCase) {
+		randomPasswordString = randomPasswordString.concat(lowerCase);
+		// console.log(randomPasswordString);
 	}
 
 	var passwordUpperCase = window.confirm(
 		"\nInclude UPPERCASE letters? \nOK for Yes or Cancel for No"
 	);
 	if (passwordUpperCase) {
-    randomPasswordString = randomPasswordString.concat(upperCase);
-    // console.log(randomPasswordString);
+		randomPasswordString = randomPasswordString.concat(upperCase);
+		// console.log(randomPasswordString);
 	}
 
 	var passwordNumbers = window.confirm(
 		"\nInclude Numbers? \nOK for Yes or Cancel for No"
 	);
 	if (passwordNumbers) {
-    randomPasswordString = randomPasswordString.concat(numbers);
-    // console.log(randomPasswordString);
+		randomPasswordString = randomPasswordString.concat(numbers);
+		// console.log(randomPasswordString);
 	}
 
 	var passwordSpecialCharacters = window.confirm(
 		"\nInlcude Special Characters? \n\nOK for Yes or Cancel for No"
 	);
-  if (passwordSpecialCharacters) {
-    randomPasswordString = randomPasswordString.concat(specialCharacters);
+	if (passwordSpecialCharacters) {
+		randomPasswordString = randomPasswordString.concat(specialCharacters);
 		// console.log(randomPasswordString);
 	} else {
 		alert("Did not meet password criteria. Please start again.");
@@ -54,22 +54,22 @@ function generatePassword() {
 	);
 
 	if (passwordLength >= 8 && passwordLength <= 128) {
-		passwordLength = length;
+		length = +passwordLength;
 	} else {
 		alert(
 			"Password length must be a minimum of 8 characters and maximum of 128 characters. Please start again."
 		);
 		return;
-  }
-  
-  for (var i = 0; i < randomPasswordString; i++) {
+	}
 
-  }
-
-};
-
-
-
+  for (var i = 0; i < randomPasswordString.length; i++) {
+		var generateRandomPassword = randomPasswordString.charAt(
+			Math.random() * length + 1
+		);
+		// randomPasswordString.charAt(i);
+	}
+	return generateRandomPassword;
+}
 
 // Write password to the #password input
 function writePassword() {
