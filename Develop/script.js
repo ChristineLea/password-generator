448ac42bd6da5a2a9bb11486f41c5af9d2ed656a
Christine Lea <christine87@live.com.au>
2023-06-10 13:11:05 +1000

    "add comment to generatePassword function"

--- Develop/script.js
+++ Develop/script.js
@@ -14,52 +14,55 @@ var length = 0;
 
 // 3. The generatePassword function is invoked.
 function generatePassword() {
+	// alert() user with instructions.
 	window.alert(
-		"PASSWORD GENERATOR: \n\nWhen prompted: \n\n1. Choose at least one password criteria to include. \n\n2. Enter password length (Min. 8 - Max. 128 characters) \n\nClick OK to Start"
+		"PASSWORD GENERATOR: \nWhen prompted: \n\n1. Choose at least one password criteria to include. \n\n2. Enter password length (Min. 8 - Max. 128 characters) \n\nClick OK to Start"
 	);
 
+	// confirm() criteria to include in password
 	var passwordSpecialCharacters = window.confirm(
 		"\nPASSWORD CRITERIA \n\nInlcude Special Characters? \n\nOK for Yes or Cancel for No"
 	);
+	// If return value is true, add the specialCharacters variable to randomPasswordString
 	if (passwordSpecialCharacters) {
 		randomPasswordString = randomPasswordString.concat(specialCharacters);
-	
 	}
 
+	// confirm() criteria to include in password
 	var passwordLowerCase = window.confirm(
 		"\nPASSWORD CRITERIA \n\nInclude lowercase letters? \n\nOK for Yes or Cancel for No"
 	);
+	// If return value is true, add the lowerCase variable to randomPasswordString
 	if (passwordLowerCase) {
 		randomPasswordString = randomPasswordString.concat(lowerCase);
-
 	}
 
+	// confirm() criteria to include in password
 	var passwordUpperCase = window.confirm(
 		"\nPASSWORD CRITERIA \n\nInclude UPPERCASE letters? \n\nOK for Yes or Cancel for No"
 	);
+	// If return value is true, add the upperCase variable to randomPasswordString
 	if (passwordUpperCase) {
 		randomPasswordString = randomPasswordString.concat(upperCase);
-
 	}
 
+	// confirm() criteria to include in password
 	var passwordNumbers = window.confirm(
 		"\nPASSWORD CRITERIA \n\nInclude Numbers? \n\nOK for Yes or Cancel for No"
 	);
+	// If return value is true, add the numbers variable to randomPasswordString
 	if (passwordNumbers) {
 		randomPasswordString = randomPasswordString.concat(numbers);
+	};
 
-	} else {
-		alert("Did not meet password criteria. Please start again.");
-		return;
-	}
-
+	// prompt() to set length of password
 	var passwordLength = window.prompt(
 		"\nPassword length must be between 8 - 128 characters. \nEnter Password Length:"
 	);
 
 	if (passwordLength >= 8 && passwordLength <= 128) {
+		// return value is a string. change to number. save to length variable
 		+passwordLength;
-		console.log(passwordLength);
 		length = passwordLength;
 	} else {
 		alert(
